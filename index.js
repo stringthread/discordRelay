cc4d42576855a65b864f4feebc20cd89d5cce00f
stringthread <string.thread@gmail.com>
2020-08-15 01:10:36 +0900

    Add UnitManager & changed bot selection system

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const {Readable}=require('stream');
 const {env}=require('process');
 const config = require('./auth.json');
 require('dotenv').config();
+const num_bots= parseInt(env.NUM_BOTS)
 
 class Silence extends Readable{
   _read(){this.push(Buffer.from([0xF8,0xFF,0xFE]))}
 }
 
 class Unit{
-  static channels=[[0,0,0],[0,0,0],[0,0,0],[0,0,0]]; //channnel-id of [in,out_1,out_2]
-  static ch2bot=new Map(); //ch2bot[ch_id]->bot id
-
   //n: 0 for in. 1 for out_1. 2 for out_2.
   get_bot_id=(n)=>{
     return this.id*3+n;
@@ -30,22 +28,98 @@ class Unit{
     return true;
   }
 
-  callback_out=(msg,n)=>{
+  fn_in=(msg,g_id,ch_id)=>{
+    let guild=this.client_in.guilds.cache.get(g_id);
+    if(!guild){
+      console.log('guild not found');
+      return;
+    }
+    let v_ch=guild.channels.cache.get(ch_id);
+    v_ch.join()
+    .then(conn => {
+      this.in_conn=conn;
+      msg.reply('ready!');
+      conn.play(new Silence,{type:'opus'});
+      //conn.on('speaking',(user,speaking)=>{console.log(`Speaking: ${user}, ${speaking}`)});
+      conn.on('speaking', (user, speaking) => {
+        if(!(user&&speaking)) return;
+        if (speaking && !this.in_user_ids.has(user.id)) {
+          // this creates a 16-bit signed PCM, stereo 48KHz PCM stream.
+          var n=(this.in_audioStream[0]?(this.in_audioStream[1]?-1:1):0)
+          console.log(`Speaking-${n}: ${user}`)
+          if(n==-1){
+            console.log('too many people speaking.');
+            return;
+          }
+          try{
+            this.in_audioStream[n] = conn.receiver.createStream(user,{mode:'opus', end:'silence'});
+            this.in_user_ids.add(user.id)
+          }catch(e){console.log(e)}
+          if(this.out_conn[n] && !this.out_conn[n].voice.speaking){
+            this.out_conn[n].play(this.in_audioStream[n],{type:'opus'})
+          }
+          // when the stream ends (the user stopped talking) tell the user
+          this.in_audioStream[n].on('end', () => {
+            console.log(`End Speaking-${n}: ${user}`);
+            this.in_audioStream[n]=null
+            this.in_user_ids.delete(user.id)
+          });
+        }
+      });
+    })
+    .catch(console.log);
+  };
+  fn_out=(msg,g_id,ch_id)=>{
+    this.clients_out.forEach((v,i)=>{
+      let guild=v.guilds.cache.get(g_id);
+      if(!guild){
+        console.log('guild not found');
+        return;
+      }
+      let v_ch=guild.channels.cache.get(ch_id);
+      v_ch.join()
+      .then(conn => {
+        msg.reply('ready!');
+        this.out_conn[i]=conn
+        conn.play(new Silence,{type:'opus'});
+        conn.on('disconnect',()=>{
+          this.out_conn[i]=null;
+        })
+        if(this.in_audioStream[i]){
+          this.out_conn[i].play(this.in_audioStream[i],{type:'opus'});
+        }
+      })
+      .catch(console.log);
+    });
+  };
+  fn_leave=(msg,flg_out)=>{
+    if(flg_out){
+      this.out_conn.forEach((v,i)=>{
+        if(v) v.disconnect();
+        this.out_conn[i]=null;
+      });
+    }else{
+      if(this.in_conn) this.in_conn.disconnect();
+      this.in_conn=null;
+    }
+  };
+
+  /*callback_out=(msg,n)=>{
     if (msg.content.startsWith(config.prefix+'out')) {
       let [command, ...channelName] = msg.content.split(" ");
       if (!msg.guild) {
         return msg.reply('no private service is available in your area at the moment. Please contact a service representative for more details.');
       }
-      const voiceChannel = msg.guild.channels.cache.find(ch => ch.name === channelName.join(" "));
-      //console.log(voiceChannel.id);
-      if (!voiceChannel || voiceChannel.type !== 'voice') {
+      const v_ch = msg.guild.channels.cache.find(ch => ch.name === channelName.join(" "));
+      //console.log(v_ch.id);
+      if (!v_ch || v_ch.type !== 'voice') {
         return msg.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
       }
-      if(!this.sel_bot(voiceChannel.id,n)) return;
-      voiceChannel.join()
+      if(!this.sel_bot(v_ch.id,n)) return;
+      v_ch.join()
         .then(conn => {
-          Unit.channels[this.id][n+1]=voiceChannel.id;
-          Unit.ch2bot.set(voiceChannel.id,this.get_bot_id(n));
+          Unit.channels[this.id][n+1]=v_ch.id;
+          Unit.ch2bot.set(v_ch.id,this.get_bot_id(n));
           msg.reply('ready!');
           this.out_conn[n]=conn
           conn.play(new Silence,{type:'opus'});
@@ -61,43 +135,43 @@ class Unit{
     }
     if(msg.content.startsWith(config.prefix+'leave')) {
       let [command, ...channelName] = msg.content.split(" ");
-      let voiceChannel = msg.guild.channels.cache.find(ch => ch.name === channelName.join(" "));
-      voiceChannel.leave();
+      let v_ch = msg.guild.channels.cache.find(ch => ch.name === channelName.join(" "));
+      v_ch.leave();
       Unit.channels[this.id][n]=0;
-      Unit.ch2bot.delete(voiceChannel.id);
+      Unit.ch2bot.delete(v_ch.id);
     }
-  }
+  }*/
 
   constructor(id){
     this.id=id
     this.client_in = new Discord.Client();
-    this.client_out_1 = new Discord.Client();
-    this.client_out_2 = new Discord.Client();
+    this.clients_out = [new Discord.Client(), new Discord.Client()];
 
     this.token_in=env["DISCORD_TOKEN_IN_"+(this.id+1)];
-    this.token_out_1=env["DISCORD_TOKEN_OUT_1_"+(this.id+1)];
-    this.token_out_2=env["DISCORD_TOKEN_OUT_2_"+(this.id+1)];
+    this.tokens_out=[env["DISCORD_TOKEN_OUT_1_"+(this.id+1)], env["DISCORD_TOKEN_OUT_2_"+(this.id+1)]];
 
-    this.in_audioStream=[null,null]
-    this.in_user_ids=new Set()
-    this.out_conn=[null,null]
+    this.in_audioStream=[null,null];
+    this.in_user_ids=new Set();
+    this.in_conn=null;
+    this.out_conn=[null,null];
+    this.fn=[this.fn_in,this.fn_out];
 
-    this.client_in.on('message', msg => {
+    /*this.client_in.on('message', msg => {
       if (msg.content.startsWith(config.prefix+'in')) {
         let [command, ...channelName] = msg.content.split(" ");
         if (!msg.guild) {
           return msg.reply('no private service is available in your area at the moment. Please contact a service representative for more details.');
         }
-        const voiceChannel = msg.guild.channels.cache.find(ch => ch.name === channelName.join(" "));
-        //console.log(voiceChannel.id);
-        if (!voiceChannel || voiceChannel.type !== 'voice') {
+        const v_ch = msg.guild.channels.cache.find(ch => ch.name === channelName.join(" "));
+        //console.log(v_ch.id);
+        if (!v_ch || v_ch.type !== 'voice') {
           return msg.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
         }
-        if(!this.sel_bot(voiceChannel.id,0)) return;
-        voiceChannel.join()
+        if(!this.sel_bot(v_ch.id,0)) return;
+        v_ch.join()
           .then(conn => {
-            Unit.channels[this.id][0]=voiceChannel.id;
-            Unit.ch2bot.set(voiceChannel.id,this.get_bot_id(0));
+            Unit.channels[this.id][0]=v_ch.id;
+            Unit.ch2bot.set(v_ch.id,this.get_bot_id(0));
             msg.reply('ready!');
             conn.play(new Silence,{type:'opus'});
             //conn.on('speaking',(user,speaking)=>{console.log(`Speaking: ${user}, ${speaking}`)});
@@ -131,19 +205,77 @@ class Unit{
       }
       if(msg.content.startsWith(config.prefix+'leave')) {
         let [command, ...channelName] = msg.content.split(" ");
-        let voiceChannel = msg.guild.channels.cache.find(ch => ch.name === channelName.join(" "));
-        voiceChannel.leave();
+        let v_ch = msg.guild.channels.cache.find(ch => ch.name === channelName.join(" "));
+        v_ch.leave();
         Unit.channels[this.id][0]=0;
-        Unit.ch2bot.delete(voiceChannel.id);
+        Unit.ch2bot.delete(v_ch.id);
       }
     });
     this.client_out_1.on('message', (msg=>this.callback_out(msg,0)).bind(this));
     this.client_out_2.on('message', (msg=>this.callback_out(msg,1)).bind(this));
+    */
 
     this.client_in.login(this.token_in);
-    this.client_out_1.login(this.token_out_1);
-    this.client_out_2.login(this.token_out_2);
+    this.clients_out.forEach((v,i)=>v.login(this.tokens_out[i]));
   }
 }
 
-var unit=[...Array(parseInt(env.NUM_BOTS))].map((_, i) => new Unit(i));
+class UnitManager{
+  constructor(units){
+    this.channels=[...Array(num_bots)].map(_=>[0,0]);//[unit_id][in/out]->ch_id
+    this.ch2bots=new Map(); //ch_id->Set(bot_group_id)
+    this.units=units;
+    this.client=units[0].client_in; //use only to get message event
+    this.client.on('message',this.msg_callback);
+  }
+  msg_callback=(msg)=>{
+    if(msg.content.startsWith(config.prefix+'in')){
+      this.fn_join(msg,0);
+    }else if(msg.content.startsWith(config.prefix+'out')){
+      this.fn_join(msg,1);
+    }else if(msg.content.startsWith(config.prefix+'leave')){
+      let [command, ...channelName] = msg.content.split(" ");
+      if (!msg.guild) {
+        return msg.reply('no private service is available in your area at the moment. Please contact a service representative for more details.');
+      }
+      const v_ch = msg.guild.channels.cache.find(ch => ch.name === channelName.join(" "));
+      if (!v_ch || v_ch.type !== 'voice') {
+        return msg.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
+      }
+      if(!this.ch2bots.has(v_ch.id)) return;
+      for(let i of this.ch2bots.get(v_ch.id)){
+        this.units[Math.floor(i/2)].fn_leave(msg,i%2);
+      }
+    }
+  };
+
+  //in_out: 0 for in, 1 for out
+  fn_join=(msg,in_out)=>{
+    let [command, ...channelName] = msg.content.split(" ");
+    if (!msg.guild) {
+      return msg.reply('no private service is available in your area at the moment. Please contact a service representative for more details.');
+    }
+    const v_ch = msg.guild.channels.cache.find(ch => ch.name === channelName.join(" "));
+    if (!v_ch || v_ch.type !== 'voice') {
+      return msg.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
+    }
+    let set_conn=this.ch2bots.get(v_ch.id);
+    let i=0;
+    for(;i<this.units.length;i++){
+      if(this.channels[i][in_out]) continue;
+      if(set_conn&&set_conn.has(2*i+1-in_out)) continue; //if partner already connected to the same ch, unavailable
+      break;
+    }
+    if(i>=this.units.length){
+      msg.reply('Bots all used...');
+      return;
+    }
+    this.channels[i][0]=v_ch.id;
+    if(!set_conn) this.ch2bots.set(v_ch.id,new Set());
+    this.ch2bots.get(v_ch.id).add(2*i+in_out);
+    this.units[i].fn[in_out](msg,msg.guild.id,v_ch.id);
+  }
+}
+
+var units=[...Array(num_bots)].map((_, i) => new Unit(i));
+var unit_m=new UnitManager(units);
